ca8654315cb68ed040b0fdcf44eeee3740dbcbe3
Brandon Fowler <brandfowl@gmail.com>
2023-10-10 15:35:18 -0400

    Use random prefixes
    electron #33460 is won't fix

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,18 +1,20 @@
 import { ipcRenderer, contextBridge, webFrame } from 'electron';
 
+const prefix = `NIMBLE_${(Math.random() + 1).toString(36).substring(2)}`;
+
 function genFuncOverrides(...funcs: string[]) {
-	return funcs.map(func => `window.${func}=window.NIMBLE_${func};`).join('');
+	return funcs.map(func => `window.${func}=window.${prefix}_${func};`).join('');
 }
 
-contextBridge.exposeInMainWorld('NIMBLE_alert', (message: unknown) => {
+contextBridge.exposeInMainWorld(`${prefix}_alert`, (message: unknown) => {
 	ipcRenderer.sendSync('web-dialog', 'alert', message);
 });
 
-contextBridge.exposeInMainWorld('NIMBLE_confirm', (message: unknown) => {
+contextBridge.exposeInMainWorld(`${prefix}_confirm`, (message: unknown) => {
 	return ipcRenderer.sendSync('web-dialog', 'confirm', message);
 });
 
-contextBridge.exposeInMainWorld('NIMBLE_prompt', (message: unknown, initial: unknown) => {
+contextBridge.exposeInMainWorld(`${prefix}_prompt`, (message: unknown, initial: unknown) => {
 	return ipcRenderer.sendSync('web-dialog', 'prompt', message, initial);
 });
 
@@ -29,5 +31,4 @@ window.addEventListener('keyup', e => {
 	);
 });
 
-// BUG: Directly override https://github.com/electron/electron/issues/33460
 webFrame.executeJavaScript(genFuncOverrides('alert', 'confirm', 'prompt'));
